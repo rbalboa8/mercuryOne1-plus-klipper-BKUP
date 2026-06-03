1cc1d86bccc3df104207c41854e066fd36e9ca66
pi <pi@mercuryone-plus-dbc2e57>
2026-06-03 18:37:49 +0100

    New backup from 03/06/26 - 18:37:49

--- printer_data/config/systemFiles/canBusMcuFirmwareAutoUpdate.sh
+++ printer_data/config/systemFiles/canBusMcuFirmwareAutoUpdate.sh
@@ -13,13 +13,19 @@ read -p "Octopus Max EZ firmware ready for flashing, please check above for any 
 python3 ~/katapult/scripts/flashtool.py -f ~/klipper/out/klipper.bin -d /dev/serial/by-id/usb-katapult_stm32h723xx_250013000F51313236343430-if00
 read -p "Octopus Max EZ firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
+
+make KCONFIG_CONFIG=orbiter_O2S_klipper.config menuconfig
+make KCONFIG_CONFIG=orbiter_O2S_klipper.config clean
+make KCONFIG_CONFIG=orbiter_O2S_klipper.config -j4
+read -p "Orbitoer O2S firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+python3 ~/katapult/scripts/flashtool.py -f ~/klipper/out/klipper.bin -d /dev/serial/by-id/usb-Klipper_stm32f072xb_Orbitool_O2S-if00
+read -p "Orbiter O2S firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+
+
 make clean KCONFIG_CONFIG=rpi_klipper.config
 make menuconfig KCONFIG_CONFIG=rpi_klipper.config
 make -j4 KCONFIG_CONFIG=rpi_klipper.config
 read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 make -j4 flash KCONFIG_CONFIG=rpi_klipper.config
-
-
-
 
 sudo service klipper start
